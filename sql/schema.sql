b1d190f9479be3259418d6be6a12b9eec168cdec
Benoît Hubert <benoithubert@gmail.com>
2018-10-12 20:23:01 +0200

    Add SQL schema and data to populate languages table

---
+++ sql/schema.sql
@@ -0,0 +1,31 @@
+create table users (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  githubId INTEGER NOT NULL,
+  login VARCHAR(100),
+  avatar VARCHAR(255)
+);
+
+create table projects (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  githubId INTEGER NOT NULL,
+  ownerGithubId INTEGER NOT NULL,
+  active BOOLEAN DEFAULT FALSE,
+  name VARCHAR(255),
+  description TEXT,
+  homepage VARCHAR(255),
+  slug VARCHAR(255),
+  htmlUrl VARCHAR(255),
+  language VARCHAR(30)
+);
+
+create table projects_languages (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  projectId INTEGER NOT NULL,
+  languageId INTEGER NOT NULL,
+  quantity INTEGER NOT NULL
+);
+
+create table languages (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(64)
+);
