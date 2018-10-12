169e73fe924d4251d042e1c9df67667beb0f93ca
Benoît Hubert <benoithubert@gmail.com>
2018-10-12 20:19:19 +0200

    Add snakeCaseKeys

--- tests/case.spec.js
+++ tests/case.spec.js
@@ -1,15 +1,24 @@
 /* global describe, it */
 const assert = require('assert');
-const { camelCaseKeys } = require('../lib/case');
+const { camelCaseKeys, snakeCaseKeys } = require('../lib/case');
 
 describe('Case transform utilities', () => {
-  it('camelCaseKeys', () => {
+  it('test camelCaseKeys', () => {
     assert.deepStrictEqual({
       htmlUrl: 'https://example.com',
       githubOwnerId: 1,
     }, camelCaseKeys({
       html_url: 'https://example.com',
       github_owner_id: 1,
+    }));
+  });
+  it('tests snakeCaseKeys', () => {
+    assert.deepStrictEqual({
+      html_url: 'https://example.com',
+      github_owner_id: 1,
+    }, snakeCaseKeys({
+      htmlUrl: 'https://example.com',
+      githubOwnerId: 1,
     }));
   });
 });
