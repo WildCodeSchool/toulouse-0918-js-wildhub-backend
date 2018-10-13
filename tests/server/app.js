defcd1551113aec9f99fe1e08ec88fc820b65f59
Benoît Hubert <benoithubert@gmail.com>
2018-10-13 05:17:05 +0200

    Configure db in test backend

--- tests/server/app.js
+++ tests/server/app.js
@@ -1,11 +1,13 @@
 // app.js
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const { db, router } = require('../../lib');
-const dbSettings = require('../../../db-settings');
-db.setup(dbSettings);
+app.use(bodyParser.json());
 
+const { db, router } = require('../../lib');
+const dbSettings = require('./db-settings');
+db.setup(dbSettings);
 
 app.use('/api', router);
 module.exports = app;
