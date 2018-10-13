82c14b19de6891f4fe20151adcadcedbb6deb8e8
Benoît Hubert <benoithubert@gmail.com>
2018-10-13 05:19:29 +0200

    Add GitHub login field to projects table, rename slug => prettyName

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,11 +9,12 @@ create table projects (
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   githubId INTEGER NOT NULL,
   ownerGithubId INTEGER NOT NULL,
+  ownerLogin VARCHAR(100),
   active BOOLEAN DEFAULT FALSE,
   name VARCHAR(255),
+  prettyName VARCHAR(255),
   description TEXT,
   homepage VARCHAR(255),
-  slug VARCHAR(255),
   htmlUrl VARCHAR(255),
   language VARCHAR(30)
 );
