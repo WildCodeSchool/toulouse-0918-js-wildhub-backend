ce110e5f91ed16f3a2f84010e17cebac75484d12
Benoît Hubert <benoithubert@gmail.com>
2018-10-13 05:53:37 +0200

    export API router as api

--- tests/server/app.js
+++ tests/server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { db, router } = require('../../lib');
+const { db, api } = require('../../lib');
 const dbSettings = require('./db-settings');
 
 const app = express();
 app.use(bodyParser.json());
 db.setup(dbSettings);
 
-app.use('/api', router);
+app.use('/api', api);
 module.exports = app;
