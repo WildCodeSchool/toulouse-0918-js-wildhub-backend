7fa6e29210e573fe56f524f61eb240a77cfa99f1
Benoît Hubert <benoithubert@gmail.com>
2018-10-13 11:57:54 +0200

    Check that JWT-encoded GitHub account id matches project's owner id

--- tests/routes/projects.spec.js
+++ tests/routes/projects.spec.js
@@ -2,11 +2,18 @@
 const assert = require('assert');
 const request = require('supertest');
 const { clear, insertLanguages, insertUser } = require('../server/db-utils');
+const jwt = require('jsonwebtoken');
 const app = require('../server/app');
 
-const dummyProject = {
+const authToken = jwt.sign({
+  accessToken: process.env.GITHUB_TOKEN,
+  login: 'bhubr',
+  githubId: 15270070
+}, 'secret');
+
+const dummyProjectBase = {
   id: 12345,
-  owner: { id: 9876, login: 'www5678' },
+  // owner: { id: 15270070, login: 'bhubr' },
   name: 'dummy-project',
   description: 'for tests purposes',
   homepage: 'https://example.com',
@@ -19,21 +26,26 @@ const dummyProject = {
   }
 };
 
-describe('Test the projects root path', () => {
+describe('Test the projects routes', () => {
   beforeEach(() => clear().then(insertLanguages));
 
-  it('It should test the GET method', () => {
+  it('It should test the GET (all) method', () => {
     request(app).get('/api/projects').then((response) => {
       assert.strictEqual(200, response.statusCode);
     });
   });
-  it('It should test the POST method', () => {
+
+  it('It should test the POST method with correct payload', () => {
+    const dummyProject = {
+      ...dummyProjectBase,
+      owner: { id: 15270070, login: 'bhubr' }
+    };
     insertUser()
       .then(
         () => request(app).post('/api/projects')
           .send(dummyProject)
           // Put a JWT in env
-          .set('Authorization', `Bearer ${process.env.JWT}`)
+          .set('Authorization', `Bearer ${authToken}`)
           .set('Content-Type', 'application/json')
           .set('Accept', 'application/json')
       )
@@ -48,6 +60,28 @@ describe('Test the projects root path', () => {
         expected.owner_login = 'www5678';
         expected.pretty_name = 'dummy-project';
         assert.deepStrictEqual({ id: 1, ...expected }, response.body);
+      });
+  });
+
+  it('It should test the POST method with owner id mismatch', () => {
+    const dummyProject = {
+      ...dummyProjectBase,
+      owner: { id: 5678, login: 'whatever' }
+    };
+    insertUser()
+      .then(
+        () => request(app).post('/api/projects')
+          .send(dummyProject)
+          // Put a JWT in env
+          .set('Authorization', `Bearer ${authToken}`)
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+      )
+      .then((response) => {
+        assert.strictEqual(403, response.statusCode);
+        assert.strictEqual(
+          "Forbidden: your GitHub account id doesn't match the project owner's id", response.body.error
+        );
       });
   });
 });
