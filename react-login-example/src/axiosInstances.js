c546f62927569131ba43d75f319c5f7bbf42b9b4
Benoît Hubert <benoithubert@gmail.com>
2018-10-13 22:35:49 +0200

    Intending to fix language-related issues

--- react-login-example/src/axiosInstances.js
+++ react-login-example/src/axiosInstances.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export const githubAxios = axios.create({
   baseURL: 'https://api.github.com',
-  timeout: 1000
+  timeout: 5000
 });
 
 export const apiAxios = axios.create({
   baseURL: 'https://wildhub.ssd1.ovh/api',
-  timeout: 1000
+  timeout: 5000
 });
--- react-login-example/src/containers/MyGitHubReposList.js
+++ react-login-example/src/containers/MyGitHubReposList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import RepositoryList from '../components/RepositoryList';
 import { githubAxios, apiAxios } from '../axiosInstances';
 
+const getLangUrl = repo => repo.languages_url.replace('https://api.github.com', '');
+
 class MyGitHubReposList extends Component {
   constructor(props) {
     super(props);
@@ -22,7 +24,21 @@ class MyGitHubReposList extends Component {
     if (login) {
       githubAxios.get(`users/${login}/repos`)
         .then(res => res.data)
-        .then(repos => this.setState({ repos }));
+        .then(
+          repos => {
+            const promises = repos.map(
+              repo => githubAxios.get(getLangUrl(repo))
+                .then(result => result.data)
+            );
+            return Promise.all(promises)
+              .then(languages => languages.map(
+                (language, idx) => Object.assign(repos[idx], { language_stat: language })
+              ))
+              .then(repos => {
+                console.log('got repos', repos);
+                this.setState({ repos })
+              });
+          });
     }
   }
 
